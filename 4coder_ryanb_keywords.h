e87d21a7de4d0a3fb29437b0a8beeb355d975ce3
Ryan Byczek <46495747+ryanbyczek@users.noreply.github.com>
2019-08-13 11:42:53 -0500

    initial commit

---
+++ 4coder_ryanb_keywords.h
@@ -0,0 +1,62 @@
+#ifndef FCODER_RYANB_KEYWORDS_H
+#define FCODER_RYANB_KEYWORDS_H
+
+/////////////////////////////////////////////////////////////////////////////
+// CUSTOM KEYWORDS                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
+PSAT("cast", CPP_TOKEN_KEY_CAST),
+
+/////////////////////////////////////////////////////////////////////////////
+// CUSTOM PRIMITIVE TYPES                                                  //
+/////////////////////////////////////////////////////////////////////////////
+
+PSAT("b32", CPP_TOKEN_KEY_TYPE),
+PSAT("ptr", CPP_TOKEN_KEY_TYPE),
+
+PSAT("s08", CPP_TOKEN_KEY_TYPE),
+PSAT("s16", CPP_TOKEN_KEY_TYPE),
+PSAT("s32", CPP_TOKEN_KEY_TYPE),
+PSAT("s64", CPP_TOKEN_KEY_TYPE),
+
+PSAT("u08", CPP_TOKEN_KEY_TYPE),
+PSAT("u16", CPP_TOKEN_KEY_TYPE),
+PSAT("u32", CPP_TOKEN_KEY_TYPE),
+PSAT("u64", CPP_TOKEN_KEY_TYPE),
+
+PSAT("f32", CPP_TOKEN_KEY_TYPE),
+PSAT("f64", CPP_TOKEN_KEY_TYPE),
+
+PSAT("int8_t",  CPP_TOKEN_KEY_TYPE),
+PSAT("int16_t", CPP_TOKEN_KEY_TYPE),
+PSAT("int32_t", CPP_TOKEN_KEY_TYPE),
+PSAT("int64_t", CPP_TOKEN_KEY_TYPE),
+
+PSAT("uint8_t",  CPP_TOKEN_KEY_TYPE),
+PSAT("uint16_t", CPP_TOKEN_KEY_TYPE),
+PSAT("uint32_t", CPP_TOKEN_KEY_TYPE),
+PSAT("uint32_t", CPP_TOKEN_KEY_TYPE),
+
+PSAT("size_t", CPP_TOKEN_KEY_TYPE),
+
+/////////////////////////////////////////////////////////////////////////////
+// CUSTOM COMPLEX TYPES                                                    //
+/////////////////////////////////////////////////////////////////////////////
+
+PSAT("u64_shape", CPP_TOKEN_KEY_TYPE),
+PSAT("f32_shape", CPP_TOKEN_KEY_TYPE),
+
+PSAT("f32x4", CPP_TOKEN_KEY_TYPE),
+PSAT("f32x8", CPP_TOKEN_KEY_TYPE),
+
+PSAT("buf", CPP_TOKEN_KEY_TYPE),
+
+PSAT("v2s", CPP_TOKEN_KEY_TYPE),
+PSAT("v2f", CPP_TOKEN_KEY_TYPE),
+PSAT("v3f", CPP_TOKEN_KEY_TYPE),
+PSAT("v4f", CPP_TOKEN_KEY_TYPE),
+PSAT("m4f", CPP_TOKEN_KEY_TYPE),
+PSAT("q4f", CPP_TOKEN_KEY_TYPE),
+
+
+#endif // FCODER_RYANB_KEYWORDS_H
